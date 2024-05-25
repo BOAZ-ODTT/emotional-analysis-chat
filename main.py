968f4b2240484e0d92c8278ce85ada7836fc6ec5
falconlee236 <falconlee236@cau.ac.kr>
2024-05-26 00:23:53 +0900

    tmp finish

--- main.py
+++ main.py
@@ -16,16 +16,22 @@ templates = Jinja2Templates(directory="templates")
 
 manager = ConnectionManager()
 
+
 class ChatRoom:
     def __init__(self, room_id):
         self.room_id = room_id
         self.manager = ConnectionManager()
+        self.users = set()  # 사용자 수 추적
 
     async def connect(self, websocket: WebSocket):
         await self.manager.connect(websocket)
+        self.users.add(websocket)  # 사용자 추가
 
     async def disconnect(self, websocket: WebSocket):
         await self.manager.disconnect(websocket)
+        self.users.remove(websocket)  # 사용자 제거
+        if not self.users:
+            del chat_rooms[self.room_id]  # 사용자가 모두 나가면 방 삭제
 
     async def broadcast(self, message: Message):
         await self.manager.broadcast(message)
@@ -34,33 +40,10 @@ class ChatRoom:
 chat_rooms = {}
 
 
-@app.websocket("/chat/{room_id}/connect")
-async def websocket_endpoint(websocket: WebSocket, room_id: str):
-    if room_id not in chat_rooms:
-        chat_rooms[room_id] = ChatRoom(room_id)
-
-    await chat_rooms[room_id].connect(websocket)
-
-    try:
-        await chat_rooms[room_id].broadcast(Message(
-            username='System', message='누군가 방에 입장했습니다.'
-        ))
-
-        while True:
-            data = await websocket.receive_text()
-            message = Message.parse_raw(data)
-
-            await chat_rooms[room_id].broadcast(message)
-    except WebSocketDisconnect:
-        await chat_rooms[room_id].disconnect(websocket)
-        await chat_rooms[room_id].broadcast(Message(username="System", message="누군가 방에서 나갔습니다."))
-
-
-@app.websocket("/chat/new/connect")
+@app.websocket("/chat/connect/new")
 async def create_new_chat_room(websocket: WebSocket):
     room_id = str(uuid.uuid4())  # 무작위로 UUID 생성
-    if room_id not in chat_rooms:
-        chat_rooms[room_id] = ChatRoom(room_id)
+    chat_rooms[room_id] = ChatRoom(room_id)
 
     await chat_rooms[room_id].connect(websocket)
 
@@ -75,8 +58,30 @@ async def create_new_chat_room(websocket: WebSocket):
 
             await chat_rooms[room_id].broadcast(message)
     except WebSocketDisconnect:
-        await chat_rooms[room_id].disconnect(websocket)
-        await chat_rooms[room_id].broadcast(Message(username="System", message="누군가 방에서 나갔습니다."))
+        chat_rooms[room_id].disconnect(websocket)
+        if room_id in chat_rooms:
+            await chat_rooms[room_id].broadcast(Message(username="System", message="누군가 방에서 나갔습니다."))
+
+
+@app.websocket("/chat/{room_id}/connect")
+async def websocket_endpoint(websocket: WebSocket, room_id: str):
+
+    await chat_rooms[room_id].connect(websocket)
+
+    try:
+        await chat_rooms[room_id].broadcast(Message(
+            username='System', message='누군가 방에 입장했습니다.'
+        ))
+
+        while True:
+            data = await websocket.receive_text()
+            message = Message.parse_raw(data)
+
+            await chat_rooms[room_id].broadcast(message)
+    except WebSocketDisconnect:
+        chat_rooms[room_id].disconnect(websocket)
+        if room_id in chat_rooms:
+            await chat_rooms[room_id].broadcast(Message(username="System", message="누군가 방에서 나갔습니다."))
 
 
 # 입장 버튼 누른 이후
@@ -97,6 +102,11 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(Message(username="System", message="누군가 방에서 나갔습니다."))
+
+
+@app.get("/chat/rooms")
+async def get_rooms():
+    return list(chat_rooms.keys())
 
 
 # root
