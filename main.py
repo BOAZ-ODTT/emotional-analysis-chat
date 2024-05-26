6549d6185077eaf405cb3c9313bb2652adf41c2d
Sangyun_LEE <falconlee236@cau.ac.kr>
2024-05-27 00:24:58 +0900

    채팅방 새로 생성하기/기존 채팅방 들어가기 기능 구현 (#7)
    * create new chat room
    
    * tmp finish
    
    * Merge from main
    
    * Update to use EmotionClassifier
    
    * add text name
    
    * bug fixed
    
    * remove initial
    
    * add other user in left side
    
    * add username to url
    
    * add username to message
    
    * add room_name in ChatRoom class
    
    * add room id dynamic
    
    ---------
    
    Co-authored-by: mokhs00 <mokhs00@naver.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import random
 import uuid
+from typing import Set, Dict
 
 from fastapi import FastAPI, Request
 from starlette.responses import HTMLResponse
@@ -8,8 +9,9 @@ from starlette.templating import Jinja2Templates
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
 from connection_manager import ConnectionManager
-from inference import predict_emotion
+from emotion_classifier import EmotionClassifier
 from message import Message
+from mock_emotion_classifier import MockEmotionClassifier
 from user_connection import UserConnection
 
 app = FastAPI()
@@ -17,35 +19,109 @@ templates = Jinja2Templates(directory="templates")
 
 connection_manager = ConnectionManager()
 
+# emotion_classifier = EmotionClassifier()
+# m1 import 이슈로 작업할 때는 MockEmotionClassifier 사용
+emotion_classifier = MockEmotionClassifier()
 
-@app.get("/health")
-async def health_check():
-    return {"status": "ok"}
+
+class ChatRoom:
+    def __init__(self, room_id):
+        self.room_id = room_id
+        self.room_name = f"room {uuid.UUID(room_id).int % 10000}"
+        self.manager = ConnectionManager()
+        self.users: Set[UserConnection] = set()  # 사용자 수 추적
+
+    async def connect(self, connection: UserConnection):
+        await self.manager.connect(connection)
+        self.users.add(connection)  # 사용자 추가
+
+    async def disconnect(self, connection: UserConnection):
+        self.manager.disconnect(connection)
+        self.users.remove(connection)  # 사용자 제거
+        if not self.users:
+            del chat_rooms[self.room_id]  # 사용자가 모두 나가면 방 삭제
+
+    async def broadcast(self, message: Message):
+        await self.manager.broadcast(message)
 
 
-@app.websocket("/chat/connect")
-async def websocket_endpoint(
-        websocket: WebSocket,
-        username: str | None = None,
-):
-    user = UserConnection(user_id=str(uuid.uuid4()), username=username, websocket=websocket)
-    await connection_manager.connect(user)
+chat_rooms: Dict[str, ChatRoom] = {}
+
+
+@app.websocket("/chat/connect/new/{username}")
+async def create_new_chat_room(websocket: WebSocket, username: str):
+    room_id = str(uuid.uuid4())  # 무작위로 UUID 생성
+    chat_rooms[room_id] = ChatRoom(room_id)
+
+    print(chat_rooms)
+
+    connection = UserConnection(
+        user_id=str(uuid.uuid4()),
+        websocket=websocket,
+        username=username,
+    )
+
+    await chat_rooms[room_id].connect(connection)
 
     try:
-        await connection_manager.broadcast(Message(
-            username='System', message='누군가 방에 입장했습니다.'
+        # 클라이언트에게 매개변수를 포함한 초기 메시지 전송
+        await chat_rooms[room_id].broadcast(Message(
+            username='Root', message=chat_rooms[room_id].room_name
+        ))
+
+        await chat_rooms[room_id].broadcast(Message(
+            username='System', message=f'{username}가 방에 입장했습니다.'
         ))
 
         while True:
             data = await websocket.receive_text()
             message = Message.parse_raw(data)
 
-            await connection_manager.broadcast(message)
-            user.add_message(message)
+            await chat_rooms[room_id].broadcast(message)
+            connection.add_message(message)
 
     except WebSocketDisconnect:
-        connection_manager.disconnect(user)
-        await connection_manager.broadcast(Message(username="System", message="누군가 방에서 나갔습니다."))
+        await chat_rooms[room_id].disconnect(connection)
+        if room_id in chat_rooms:
+            await chat_rooms[room_id].broadcast(Message(username="System", message=f"{username}가 방에서 나갔습니다."))
+
+
+@app.websocket("/chat/{room_id}/connect/{username}")
+async def websocket_endpoint(websocket: WebSocket, room_id: str, username: str):
+    connection = UserConnection(
+        user_id=str(uuid.uuid4()),
+        websocket=websocket,
+        username=username,
+    )
+
+    await chat_rooms[room_id].connect(connection)
+
+    try:
+        # 클라이언트에게 매개변수를 포함한 초기 메시지 전송
+        await chat_rooms[room_id].broadcast(Message(
+            username='Root', message=chat_rooms[room_id].room_name
+        ))
+
+        await chat_rooms[room_id].broadcast(Message(
+            username='System', message=f'{username}가 방에 입장했습니다.'
+        ))
+
+        while True:
+            data = await websocket.receive_text()
+            message = Message.parse_raw(data)
+
+            await chat_rooms[room_id].broadcast(message)
+            connection.add_message(message)
+
+    except WebSocketDisconnect:
+        await chat_rooms[room_id].disconnect(connection)
+        if room_id in chat_rooms:
+            await chat_rooms[room_id].broadcast(Message(username="System", message=f"{username}가 방에서 나갔습니다."))
+
+
+@app.get("/chat/rooms")
+async def get_rooms():
+    return [value.room_name for value in chat_rooms.values()]
 
 
 # root
@@ -57,36 +133,44 @@ async def root(request: Request):
     )
 
 
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
+
+
 async def broadcast_emotion_message():
     while True:
         try:
             await asyncio.sleep(20)
 
-            connections = connection_manager.get_connections()
-            if not connections:
-                continue
+            for key in chat_rooms.keys():
+                manager = chat_rooms[key].manager
 
-            chose_connection: UserConnection = random.choice(connections)
+                connections = manager.get_connections()
+                if not connections:
+                    continue
 
-            messages = chose_connection.get_messages()
-            if len(messages) == 0:
-                await connection_manager.broadcast(
+                chose_connection: UserConnection = random.choice(connections)
+
+                messages = chose_connection.get_messages()
+                if len(messages) == 0:
+                    await manager.broadcast(
+                        Message(
+                            username="System",
+                            message=f"메시지를 입력해보세요!",
+                        )
+                    )
+                    continue
+                last_message = messages[-1].message
+
+                emotion_text = emotion_classifier.classify(last_message)
+
+                await manager.broadcast(
                     Message(
                         username="System",
-                        message=f"메시지를 입력해보세요!",
+                        message=f"{chose_connection.username}의 {emotion_text} 느껴집니다.",
                     )
                 )
-                continue
-            last_message = messages[-1].message
-
-            emotion_text = predict_emotion(last_message)
-
-            await connection_manager.broadcast(
-                Message(
-                    username="System",
-                    message=f"{chose_connection.username}의 {emotion_text} 느껴집니다.",
-                )
-            )
 
         except Exception as e:
             print(e)
