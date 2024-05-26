eb3e3360143327a15dbad3177e969cea803197e2
mokhs00 <mokhs00@naver.com>
2024-05-27 01:46:10 +0900

    Update message에 message_type 필드 추가

--- message.py
+++ message.py
@@ -1,6 +1,15 @@
+from enum import Enum
+from typing import Optional
+
 from pydantic import BaseModel
+
+
+class MessageType(Enum):
+    USER_MESSAGE = "USER_MESSAGE"
+    SYSTEM_MESSAGE = "SYSTEM_MESSAGE"
 
 
 class Message(BaseModel):
     username: str
     message: str
+    message_type: Optional[MessageType] = None
