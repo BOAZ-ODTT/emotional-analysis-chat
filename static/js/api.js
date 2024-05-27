2112cb1abb63c9710eac2d6ed3fd9de52bdad9b7
mokhs00 <mokhs00@naver.com>
2024-05-27 22:19:35 +0900

    Refactor api 관련 js 코드 정리

---
+++ static/js/api.js
@@ -0,0 +1,24 @@
+const apiEndpoint = `${window.location.origin}/api/v1`;
+const socketEndpoint = apiEndpoint.replace('http', 'ws');
+
+async function fetchChatRooms() {
+    const response = await fetch(`${apiEndpoint}/chat/rooms`);
+
+    return await response.json();
+}
+
+async function createChatRoom(name) {
+    const response = await fetch(`${apiEndpoint}/chat/rooms/new`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name}),
+    });
+
+    return await response.json();
+}
+
+async function connectChatRoomSocket(roomId, username) {
+    return new WebSocket(`${socketEndpoint}/chat/${roomId}/connect/${username}`);
+}
